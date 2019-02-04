d8fbb14d81f43849ef0657585c1c59f3515325ac
Gert Adamson <adamson.gert@hotmail.com>
2019-02-04 23:24:46 +0200

    Add abstraction to table classes

--- application/items/models.py
+++ application/items/models.py
@@ -1,9 +1,8 @@
 from application import db
+from application.models import BaseWithName
 
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    name = db.Column(db.String(144), nullable=False)
+class Item(BaseWithName):
+    
     best_before = db.Column(db.Date, nullable=True)
     used = db.Column(db.Boolean, nullable=False)
     expired = db.Column(db.Boolean, nullable=False)
